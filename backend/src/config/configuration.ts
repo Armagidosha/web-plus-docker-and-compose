cacd2c582dfd1f7aa5787e48eea108606f6bc056
Armagidosha <armgidosha@list.ru>
2024-04-01 15:38:05 +0300

    update environment vars

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,17 +1,17 @@
 export default () => ({
   db: {
-    type: process.env.DBTYPE,
-    username: process.env.DBUSERNAME,
-    password: process.env.DBPASSWORD,
-    name: process.env.DBNAME,
+    type: process.env.DB_TYPE,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    name: process.env.POSTGRES_DB,
   },
   jwt: {
     secret: process.env.JWTSECRET,
     ttl: process.env.TTL + "d",
   },
   host: {
-    port: parseInt(process.env.PORT) || 5432,
-    hostname: process.env.HOST || "127.0.0.1",
+    port: parseInt(process.env.POSTGRES_PORT) || 5432,
+    hostname: process.env.POSTGRES_HOST || "127.0.0.1",
   },
   mode: process.env.MODE || "dev",
 });
